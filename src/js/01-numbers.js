13efe3b4312cc33ff73f05d609e200d18c36550c
Maria Ozamiz <ozamiz.m@gmail.com>
2020-06-30 09:34:57 +0200

    add function checkGuessNumber first conditional

--- src/js/01-numbers.js
+++ src/js/01-numbers.js
@@ -1,11 +1,32 @@
 'use strict';
 
-//Acceder al HTML
+//Access to HTML
 
 const numberField = document.querySelector('.js-input');
-const numberGuess = numberField.value;
 const button = document.querySelector('.js-button');
 const hintField = document.querySelector('js-hint');
-const hintText = hintField.innerHTML;
+//const hintText = hintField.innerHTML;
 const attemptsField = document.querySelector('js-attempts');
-const attemptsNumber = attemptsField.innerHTML;
+//const attemptsNumber = attemptsField.innerHTML;
+
+//Función num. aleatorio
+let randomNumber;
+function getRandomNumber(max) {
+    randomNumber = Math.ceil(Math.random() * max);
+    return randomNumber;
+}
+console.log(getRandomNumber(100));
+
+//Función comparar
+function checkGuessNumber() {
+    let guessNumber = parseInt(numberField.value);
+    console.log(`Esto es lo que he escrito ${numberField.value}`);
+    console.log(`Este es el número aleatorio: ${randomNumber}`);
+    console.log(guessNumber);
+    console.log(randomNumber);
+    if (guessNumber === randomNumber) {
+        console.log('¡HURRA!');
+    }
+}
+
+button.addEventListener('click', checkGuessNumber);
